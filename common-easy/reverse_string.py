b260194198647c4f2dd7c8140ec849ddb6ca91d5
Anubhav Ranjan <60106478+Mr-AB007@users.noreply.github.com>
2025-03-27 20:58:56 +0530

    reverse string

---
+++ common-easy/reverse_string.py
@@ -0,0 +1,23 @@
+"""
+    Problem Statement:
+    Given a list of characters, reverse the list in-place.
+
+    Example:
+    Input: ['h', 'e', 'l', 'l', 'o']
+    Output: ['o', 'l', 'l', 'e', 'h']
+    """
+
+def reverse_string(s:list):
+    left = 0
+    right = len(s) - 1
+    while left < right:
+        s[left], s[right] =  s[right] , s[left] #swaping
+        left += 1
+        right -= 1
+
+# Utility Main
+if __name__ == "__main__":
+    s = ['h', 'e', 'l', 'l', 'o']
+    print("Original:", s)
+    reverse_string(s)
+    print("Reversed:", s)
