3c71eae1874c0e92e7f54fcc3b09748eab449d1c
Anubhav Ranjan <60106478+Mr-AB007@users.noreply.github.com>
2025-04-08 22:12:09 +0530

    find nth node from end

--- LinkedList/remove_nth_from_end.py
+++ LinkedList/remove_nth_from_end.py
@@ -1,26 +1,30 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+from typing import Optional
 
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        
-        res = ListNode(0,head)
-        
-        fast = res
-        slow = res
 
-        # for i in range(0,n+1):
-        #     fast = fast.next
+class ListNode:
+    """Definition for a singly-linked list node."""
+    def __init__(self, val=0, next=None):
+        self.val = val  # Node value
+        self.next = next  # Pointer to the next node
 
-        for _ in range(n + 1):
-            fast = fast.next
-        while fast:
-            fast = fast.next
-            slow = slow.next
+def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
 
-        slow.next = slow.next.next
+    res = ListNode(0,head)
 
-        return res.next
-        
+    fast = res
+    slow = res
+
+    # for i in range(0,n+1):
+    #     fast = fast.next
+
+    for _ in range(n + 1):
+        fast = fast.next
+    while fast:
+        fast = fast.next
+        slow = slow.next
+
+    slow.next = slow.next.next
+
+    return res.next
+
