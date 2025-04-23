5f3904fb73bae598016a1675a15be6d8561631fc
Anubhav Ranjan <60106478+Mr-AB007@users.noreply.github.com>
2025-04-23 17:07:08 +0530

    Update continuous_array.py

--- array/continuous_array.py
+++ array/continuous_array.py
@@ -1,16 +1,20 @@
+
+
 class continuous_array:
     def findMaxLength(self, nums: List[int]) -> int:
-        prefix_fre = {}
-        largest = 1
+        prefix_fre = {0: -1}
+        largest = 0
         prefix_sum = 0
 
-        for i in range(0, len(nums)):
-            prefix_sum += 2*nums[i] - 1
+        for i in range(len(nums)):
+            prefix_sum += 2 * nums[i] - 1
 
-            if prefix_fre.get(prefix_sum):
+            if prefix_fre.get(prefix_sum) is not None:
                 index = i - prefix_fre[prefix_sum]
                 if index > largest:
                     largest = index
             else:
                 prefix_fre[prefix_sum] = i
+
         return largest
+
